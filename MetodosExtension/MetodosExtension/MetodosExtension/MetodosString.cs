21eee124e63d69fd14f1c903f7720e3cdf04f9de
GabyVale <48566458+GabyVale@users.noreply.github.com>
2019-03-21 18:16:26 -0600

    Add files via upload

---
+++ MetodosExtension/MetodosExtension/MetodosExtension/MetodosString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosExtension
+{
+    static class MetodosString
+    {
+        public static bool Digito (this string Cadena)
+        {
+            List<char> ListaChars = Cadena.ToList();
+            bool valorBool = ListaChars.All(delegado => Char.IsDigit(delegado));
+            return valorBool;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Cadena"></param>
+        /// <param name="resultado"></param>
+        /// <returns></returns>
+        public static bool ContarLetras( this List<char> Cadena, Func<char, bool> resultado)
+        {
+            #region
+            List<char> ListaChars = Cadena.ToList();
+            #endregion
+            bool valorBool = ListaChars.All(delegado2 => resultado(delegado2));
+            return valorBool;
+        }
+
+
+        
+
+    }
+}
---
+++ MetodosExtension/MetodosExtension/MetodosExtension/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Oracion="1234tt"; //Cadena a revisar
+            //char Oracion2;
+            bool resultado = Oracion.Digito(); //Digito ya es un metodo de extension creado por mi
+            bool resultado2 = Oracion.ToList().ContarLetras(char.IsLetter);
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
